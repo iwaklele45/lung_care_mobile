c7b6ab4867c0a09a7f81efa5d4c16c3bb2751130
RexCronous <renza.syn@gmail.com>
2026-05-06 20:58:31 +0700

    add: implement register page with user input fields and navigation

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,26 +5,18 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:lung_care_mobile/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
+  testWidgets('register page renders expected sections', (
+    WidgetTester tester,
+  ) async {
     await tester.pumpWidget(const MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.text('Daftar Akun Baru'), findsOneWidget);
+    expect(find.text('Mulai Perjalanan\nSehat'), findsOneWidget);
+    expect(find.text('Buat Akun'), findsOneWidget);
+    expect(find.text('Daftar dengan Google'), findsOneWidget);
   });
 }
