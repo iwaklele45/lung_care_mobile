e765e1a675a089e48fe66fe530daa0c6d5b5c4b3
IwakLele <108775169+iwaklele45@users.noreply.github.com>
2026-06-06 15:23:58 +0700

    test: update widget test to initialize MyApp with LocaleProvider

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,18 +1,14 @@
 // This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter_test/flutter_test.dart';
 import 'package:lung_care_mobile/main.dart';
+import 'package:lung_care_mobile/src/core/locale/locale_provider.dart';
 
 void main() {
   testWidgets('register page renders expected sections', (
     WidgetTester tester,
   ) async {
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(MyApp(localeProvider: LocaleProvider()));
 
     expect(find.text('Daftar Akun Baru'), findsOneWidget);
     expect(find.text('Mulai Perjalanan\nSehat'), findsOneWidget);
